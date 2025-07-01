91548d2106ba9900490d0a4a22e4befa76c81a4e
Joshua Kennedy <Joshua.Kennedy@fele.com>
2025-07-01 09:16:55 -0400

    update api naming

--- CampaignManager.WebApp/Controllers/UserController.cs
+++ CampaignManager.WebApp/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CampaignManager.WebApp.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("backendApi/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
