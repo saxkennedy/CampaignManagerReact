a1f8fb592d5d5bbd5ae78b5fc360a16d67247765
Joshua Kennedy <Joshua.Kennedy@fele.com>
2025-07-01 11:38:06 -0400

    controllerdeeptest

--- CampaignManager.WebApp/Controllers/UserController.cs
+++ CampaignManager.WebApp/Controllers/UserController.cs
@@ -1,5 +1,4 @@
 using CampaignManager.Services.Models;
-using CampaignManager.Services.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -10,18 +9,58 @@ namespace CampaignManager.WebApp.Controllers
     public class UserController : ControllerBase
     {
         //private readonly IUserService UserService;
-        private readonly ITemporaryLoginService TemporaryLoginService;
-        public UserController(/*IUserService userService,*/ ITemporaryLoginService temporaryLoginService)
-        {
-            //UserService = userService;
-            TemporaryLoginService = temporaryLoginService;
-        }
         [HttpGet("getUser/{email}/{password}")]
         public async Task<UserResponse> GetUser(string email, string password)
         {
-            //UserResponse response= await UserService.GetUser(email, password);
-            UserResponse response = await TemporaryLoginService.GetUser(email, password);
-            return response;
+            try
+            {
+                if (email == "player@endersgame.com" && password == "pleasedontabuse")
+                {
+                    var playerContentAccess = new List<ContentAccess>()
+                    //make a new list of ContentAccess items
+                    {
+                        new ContentAccess() { Name = "RealmsBetwixt", DisplayName = "Realms Betwixt Campaign Access", HasAccess = true },
+                        new ContentAccess() { Name = "Public", DisplayName = "Public Campaign Details", HasAccess = true },
+                        new ContentAccess() { Name = "Private", DisplayName = "Private Campaign Details", HasAccess = false }
+                    };
+                    return new UserResponse()
+                    {
+                        Id = Guid.Empty,
+                        Email = email,
+                        FirstName = "Guest",
+                        LastName = "McGuest",
+                        Persona = "Player",
+                        ContentAccess = playerContentAccess
+                    };
+                }
+                if (email == "admin@endersgame.com" && password == "vampjuice123")
+                {
+                    var adminContentAccess = new List<ContentAccess>()
+                    //make a new list of ContentAccess items
+                    {
+                        new ContentAccess() { Name = "RealmsBetwixt", DisplayName = "Realms Betwixt Campaign Access", HasAccess = true },
+                        new ContentAccess() { Name = "Public", DisplayName = "Public Campaign Details", HasAccess = true },
+                        new ContentAccess() { Name = "Private", DisplayName = "Private Campaign Details", HasAccess = true }
+                    };
+                    return new UserResponse()
+                    {
+                        Id = Guid.Empty,
+                        Email = email,
+                        FirstName = "Ender",
+                        LastName = "Wiggin",
+                        Persona = "Admin",
+                        ContentAccess = adminContentAccess
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while retrieving the user.", ex);
+            }
         }
     }
 }
