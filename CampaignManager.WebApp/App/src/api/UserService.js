0a179f11c272c5a8f1bdaf5daa3f1bbf5072b6f9
Joshua Kennedy <Joshua.Kennedy@fele.com>
2025-07-01 13:34:16 -0400

    wipperuni

--- CampaignManager.WebApp/App/src/api/UserService.js
+++ CampaignManager.WebApp/App/src/api/UserService.js
@@ -2,16 +2,31 @@ import Api from './Api';
 class UserService {
     callbacks = [];
 
-    async CreateUser(user) {
-        let url = '/api/user/createUser';
-        return Api.fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({ Email: user.Email, Password: user.Password, FirstName: user.FirstName, LastName: user.LastName })
-        });
-    }
-
+    //async CreateUser(user) {
+    //    let url = '/api/user/createUser';
+    //    return Api.fetch(url, {
+    //        method: 'POST',
+    //        body: JSON.stringify({ Email: user.Email, Password: user.Password, FirstName: user.FirstName, LastName: user.LastName })
+    //    });
+    //}
+    //
+    //async GetUser(email, password) {
+    //    return Api.fetch(`/api/user/getUser/${email}/${password}`);
+    //}
     async GetUser(email, password) {
-        return Api.fetch(`/api/user/getUser/${email}/${password}`);
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            console.log("Success:", result.message);
+        }
+        else {
+            console.error("Error:", result.message);
+        }
     }
 }
 
