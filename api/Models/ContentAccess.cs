98fff38e54b65ecc8f45df5456be3338301ad530
Joshua Kennedy <Joshua.Kennedy@fele.com>
2025-07-01 14:59:31 -0400

    early wip

---
+++ api/Models/ContentAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models
+{
+    public class ContentAccess
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public bool HasAccess { get; set; } = false;
+    }
+}
