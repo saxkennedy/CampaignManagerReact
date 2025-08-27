b496491abacd5d4472687d7940aa665e4e2c1194
Joshua Kennedy <saxkennedy@gmail.com>
2025-08-27 12:59:55 -0400

    wip

--- CampaignManager.WebApp/App/src/api/UserService.js
+++ CampaignManager.WebApp/App/src/api/UserService.js
@@ -2,17 +2,15 @@ import Api from './Api';
 class UserService {
     callbacks = [];
 
-    //async CreateUser(user) {
-    //    let url = '/api/user/createUser';
-    //    return Api.fetch(url, {
-    //        method: 'POST',
-    //        body: JSON.stringify({ Email: user.Email, Password: user.Password, FirstName: user.FirstName, LastName: user.LastName })
-    //    });
-    //}
-    //
-    //async GetUser(email, password) {
-    //    return Api.fetch(`/api/user/getUser/${email}/${password}`);
-    //}
+    async CreateUser(user) {
+        let url = '/api/createUser';
+        return Api.fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ Email: user.Email, Password: user.Password, FirstName: user.FirstName, LastName: user.LastName })
+        });
+    }
+    
     async GetUser(email, password) {
         const response = await fetch('/api/login', {
             method: 'POST',
